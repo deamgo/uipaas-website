c4dd3c96bf7522a0a7b31db032aad3810e248188
iLeepop <97011124+iLeepop@users.noreply.github.com>
2023-11-22 14:50:20 +0800

    fixed:move all into front (#3)

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,10 @@ function App() {
         <div className='head'></div>
       </header>
       <main>
-        <div className='container'></div>
+        <div className='container'>
+          <span className='title'>AI原生<br/>面向未来的可编排数字化平台</span>
+          <span className='gary'>软件开发团队的敏捷交付神器</span>
+        </div>
         <div className='container'></div>
         <div className='container'></div>
         <div className='tail'></div>
