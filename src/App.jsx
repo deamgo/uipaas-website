5addeb7655098d547637b552e6bc0235a5fdb86b
iLeepop <zihao02325@gmail.com>
2023-11-22 14:08:16 +0800

    Directory Sturcture Init

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <header>
+        <div className='head'></div>
+      </header>
+      <main>
+        <div className='container'></div>
+        <div className='container'></div>
+        <div className='container'></div>
+        <div className='tail'></div>
+      </main>
+      <footer>
+        <div className='foot'></div>
+      </footer>
+    </>
+  )
+}
+
+export default App
