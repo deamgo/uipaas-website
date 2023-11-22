6ca381460f7ec6f61ee8d13e57780a88fa753226
iLeepop <zihao02325@gmail.com>
2023-11-22 19:40:30 +0800

    type text svg

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,16 +1,36 @@
-import './App.css'
+import { useState, useEffect } from 'react'
+import './App.less'
+import featureSVG from './assets/feature.svg'
 
 function App() {
+
+  const [__HG, setHG] = useState(1440);
+  
+  useEffect(() => {
+    let HG = window.screen.height
+    setHG(HG)
+  },[])
+
 
   return (
     <>
       <header>
-        <div className='head'></div>
+        <div 
+          className='head'></div>
       </header>
       <main>
         <div className='container'>
-          <span className='title'>AI原生<br/>面向未来的可编排数字化平台</span>
-          <span className='gary'>软件开发团队的敏捷交付神器</span>
+          <div className='text_dsp_block'>
+            <span className='title'>AI原生<br/>面向未来的可编排数字化平台</span>
+            <span className='gary'>软件开发团队的敏捷交付神器</span>
+            <span className='mark'>即将上线...</span>
+            <button className='reserveButton'>
+              <span>立即预约体验</span>
+            </button>
+            <div className='svg_block'>
+              <img src={featureSVG} alt='feature' />
+            </div>
+          </div>
         </div>
         <div className='container'></div>
         <div className='container'></div>
