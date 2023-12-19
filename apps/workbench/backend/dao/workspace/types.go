077df0a37a4c2361fd430dd4833e6c5de5bc7790
RedCrazyGhost <zhan940842903@163.com>
2023-12-19 11:32:19 +0800

    fix: use en comment

--- apps/workbench/backend/dao/workspace/types.go
+++ apps/workbench/backend/dao/workspace/types.go
@@ -5,19 +5,19 @@ import (
 )
 
 type WorkspaceDO struct {
-	Id          string `gorm:"type:char(6);size 6;primary_key;comment:'工作空间ID'"`
-	Name        string `gorm:"type:char(20);size 20;not null;comment:'工作空间名称'"`
-	Logo        string `gorm:"type:varchar(255);size 255;not null;comment:'图标地址'"`
-	Lable       string `gorm:"type:varchar(255);size 255;not null;default:'';comment:'短描述'"`
-	Description string `gorm:"type:varchar(1023);size 1023;not null;default:'';comment:'长描述'"`
+	Id          string `gorm:"type:char(6);size 6;primary_key;comment:'workspace ID'"`
+	Name        string `gorm:"type:char(20);size 20;not null;comment:'workspace Name'"`
+	Logo        string `gorm:"type:varchar(255);size 255;not null;comment:'logo'"`
+	Lable       string `gorm:"type:varchar(255);size 255;not null;default:'';comment:'lable'"`
+	Description string `gorm:"type:varchar(1023);size 1023;not null;default:'';comment:'description'"`
 
-	CreatedBy uint64    `gorm:"type:bigint;default:0;not null;comment:'创建人';"`
-	CreatedAt time.Time `gorm:"comment:'创建时间';"`
-	UpdatedBy uint64    `gorm:"type:bigint;default:0;not null;comment:'最后一次更新的开发者';"`
-	UpdatedAt time.Time `gorm:"comment:'更新时间';"`
-	DeletedBy uint64    `gorm:"type:bigint;default:0;comment:'删除人';"`
-	DeletedAt time.Time `gorm:"default:null';comment:'删除时间';"`
-	IsDeleted uint8     `gorm:"type:tinyint;default:0;not null;index;comment:'逻辑删除 0-未删除 1-已删除'"`
+	CreatedBy uint64    `gorm:"type:bigint;default:0;not null;comment:'creator';"`
+	CreatedAt time.Time `gorm:"comment:'creation time';"`
+	UpdatedBy uint64    `gorm:"type:bigint;default:0;not null;comment:'The last person to update the data';"`
+	UpdatedAt time.Time `gorm:"comment:'update time';"`
+	DeletedBy uint64    `gorm:"type:bigint;default:0;comment:'Deleting people';"`
+	DeletedAt time.Time `gorm:"default:null';comment:'Delete time';"`
+	IsDeleted uint8     `gorm:"type:tinyint;default:0;not null;index;comment:'Logical deletion 0-not deleted 1-deleted'"`
 }
 
 func (WorkspaceDO) TableName() string {
